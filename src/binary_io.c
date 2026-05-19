f698c984db7ef8aefa879e19d179bbf8a82375c3
Daniel-Sinkin <danielsinkin97@gmail.com>
2026-05-19 14:05:26 +0200

    remove experimental c89 code (migrated to c89-sandbox)

--- src/binary_io.c
+++
@@ -1,6 +0,0 @@
-#include "binary_io.h"
-
-int binary_io_version(void)
-{
-    return 1;
-}
--- src/binary_io.h
+++
@@ -1,8 +0,0 @@
-#ifndef DANS_BINARY_IO_H
-#define DANS_BINARY_IO_H
-
-#include <stddef.h>
-
-int binary_io_version(void);
-
-#endif /* DANS_BINARY_IO_H */
